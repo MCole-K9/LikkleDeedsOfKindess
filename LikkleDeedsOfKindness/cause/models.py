06d0c8f14dba90419d28583345a004aa6a0fa132
Matthew Cole <m.b.cole300@gmail.com>
2022-03-19 17:58:23 -0500

    Added Cause and Event Models

--- LikkleDeedsOfKindness/cause/models.py
+++ LikkleDeedsOfKindness/cause/models.py
@@ -1,3 +1,19 @@
+
+from sysconfig import parse_config_h
 from django.db import models
 
 # Create your models here.
+
+class Cause(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    content = models.TextField()
+    image = models.ImageField(upload_to="static/images")
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    cause = models.ForeignKey(Cause, on_delete=models.CASCADE, null=True)
+
