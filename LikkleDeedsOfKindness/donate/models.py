e27d974c66541abcf72ecfb52403124e5a4d00fa
Matthew Cole <m.b.cole300@gmail.com>
2022-03-19 18:00:07 -0500

    Added Donation and Donor Models

--- LikkleDeedsOfKindness/donate/models.py
+++ LikkleDeedsOfKindness/donate/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cause.models import Cause
 
 # Create your models here.
+class Donor(models.Model):
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=60)
+    phone_number = models.CharField(max_length=20)
+
+
+class Donation(models.Model):
+    amount = models.FloatField()
+    donor = models.ForeignKey(Donor, on_delete=models.CASCADE)
+    cause = models.ForeignKey(Cause, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField(auto_now=True)
+
+
