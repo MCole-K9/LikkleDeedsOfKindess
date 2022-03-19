c6bda53d5cc83ede44ffdd72378f01733776c874
Matthew Cole <m.b.cole300@gmail.com>
2022-03-19 18:19:37 -0500

    Added Project and ProjectImages models

--- LikkleDeedsOfKindness/project/models.py
+++ LikkleDeedsOfKindness/project/models.py
@@ -1,3 +1,18 @@
+from email.mime import image
 from django.db import models
+from cause.models import Cause
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    content = models.TextField()
+    cause = models.ForeignKey(Cause, on_delete=models.CASCADE)
+
+
+class ProjectImages(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="static/images")
+    caption = models.CharField(max_length=225)
