061f9cb31f8d2457263aca205498cf29902e5d82
Matthew Cole <m.b.cole300@gmail.com>
2022-03-19 19:52:07 -0500

    Volunteer, General Volunteer Admin Volunteer added

--- LikkleDeedsOfKindness/volunteer/models.py
+++ LikkleDeedsOfKindness/volunteer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cause.models import Event
 
 # Create your models here.
+class Volunteer(models.Model):
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=60)
+    phone_number = models.CharField(max_length=20)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
+
+class GeneralVolunteer(models.Model):
+    volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
+
+class AdminVolunteer(models.Model):
+    volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
