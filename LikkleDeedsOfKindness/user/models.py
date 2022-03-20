6b22370eb06c3a8338c94a90b25f4cb6644e90af
Matthew Cole <m.b.cole300@gmail.com>
2022-03-19 19:50:31 -0500

    User model added

--- LikkleDeedsOfKindness/user/models.py
+++ LikkleDeedsOfKindness/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=60)
+    phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=225)
