f2d0f31f44d47b6112cd8188db6ce6e46e2459e7
Matthew Cole <m.b.cole300@gmail.com>
2022-03-20 21:16:54 -0500

    Set nav bar to fixed-top on scroll

--- LikkleDeedsOfKindness/static/js/main.js
+++ LikkleDeedsOfKindness/static/js/main.js
@@ -12,3 +12,23 @@ $("#join-us").click(()=>{
 $("#get-involved").click(()=>{
     location.href = "/"
 });
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll >= 50) {
+        
+        $("nav").addClass("bg-white");
+        $("nav").removeClass("navbar-dark")
+        $("nav").addClass("navbar-light");
+        $("nav").addClass("fixed-top");
+
+    }else{
+
+        $("nav").removeClass("bg-white");
+        $("nav").addClass("navbar-dark")
+        $("nav").removeClass("fixed-top");
+        $("nav").RemoveClass("navbar-light");
+    }
+});
