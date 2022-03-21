13f854734f64373fb938eb93cb6e7f0b46442514
Matthew Cole <m.b.cole300@gmail.com>
2022-03-21 15:25:24 -0500

    added about view

--- LikkleDeedsOfKindness/LikkleDeedsOfKindness/views.py
+++ LikkleDeedsOfKindness/LikkleDeedsOfKindness/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def Index(request):
+
+def index(request):
     return render(request, "index.html", {})   
+
+def about(request):
+    return render(request, "about.html", {})
