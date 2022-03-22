faf7a8d334286c34afd1f5bf2e7346a323ce11f0
Matthew Cole <m.b.cole300@gmail.com>
2022-03-22 18:18:49 -0500

    added events view

--- LikkleDeedsOfKindness/cause/views.py
+++ LikkleDeedsOfKindness/cause/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def cause(request):
-    return render(request, "cause.html", {})   
+    return render(request, "cause.html", {})
+
+def events(request):
+    return render(request, "events.html", {}) 
