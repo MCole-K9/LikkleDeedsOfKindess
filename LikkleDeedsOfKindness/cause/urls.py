63dc16c3109f2c07e4b57d735a9bb5df9ced1b93
Matthew Cole <m.b.cole300@gmail.com>
2022-03-24 00:27:23 -0500

    added path to detail view

--- LikkleDeedsOfKindness/cause/urls.py
+++ LikkleDeedsOfKindness/cause/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "cause"
 urlpatterns = [
     path("", views.cause, name="Cause"),
-    path("Events", views.events, name="Events")
+    path("Events", views.events, name="Events"),
+    path("Detail", views.cause_detail, name="Detail")
 ]
