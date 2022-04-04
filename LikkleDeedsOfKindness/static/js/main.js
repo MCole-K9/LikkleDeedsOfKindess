5283846626ddb498164a51d5901fcec41c2ef82d
Matthew Cole <m.b.cole300@gmail.com>
2022-04-03 19:59:20 -0500

    Added functionality to show input on other click

--- LikkleDeedsOfKindness/static/js/main.js
+++ LikkleDeedsOfKindness/static/js/main.js
@@ -3,22 +3,51 @@ function goToLocation(url) {
     location.href = url;
 }
 
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+// $(window).scroll(function() {    
+//     var scroll = $(window).scrollTop();
 
-     //>=, not <=
-    if (scroll >= 80) {
+//      //>=, not <=
+//     if (scroll >= 80) {
         
-        $("nav").addClass("bg-white");
-        $("nav").removeClass("navbar-dark")
-        $("nav").addClass("navbar-light");
-        $("nav").addClass("sticky-top");
+//         $("nav").addClass("bg-white");
+//         $("nav").removeClass("navbar-dark")
+//         $("nav").addClass("navbar-light");
+//         $("nav").addClass("sticky-top");
 
-    }else{
+//     }else{
 
-        $("nav").removeClass("bg-white");
-        $("nav").addClass("navbar-dark")
-        $("nav").removeClass("sticky-top");
-        $("nav").removeClass("navbar-light");
-    }
+//         $("nav").removeClass("bg-white");
+//         $("nav").addClass("navbar-dark")
+//         $("nav").removeClass("sticky-top");
+//         $("nav").removeClass("navbar-light");
+//     }
+// });
+
+// $("#collapse-other").;
+
+
+let radioBtns = document.querySelectorAll('input[type="radio"]');
+
+
+
+radioBtns.forEach(radioBtn =>{
+
+    radioBtn.addEventListener("change", ()=>{
+
+    
+        
+        if(radioBtn.checked && radioBtn.value == 0){
+
+            console.log(radioBtn.value);
+            document.getElementById("collapse-other").classList.add("d-block");
+            document.getElementById("collapse-other").classList.remove("d-none");
+
+        }else{
+            
+            document.getElementById("collapse-other").classList.add("d-none");
+            document.getElementById("collapse-other").classList.remove("d-block");
+        }
+    })
+
+    
 });
