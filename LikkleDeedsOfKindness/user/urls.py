5c82128e3541491d729e3402f3984dc1f858f31e
Matthew Cole <m.b.cole300@gmail.com>
2022-05-19 22:45:54 -0500

    Created Volunteers Page

--- LikkleDeedsOfKindness/user/urls.py
+++ LikkleDeedsOfKindness/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import URLPattern, path
 from . import views
 
-name = "user"
+app_name = "user"
 
 urlpatterns = [
-    path("Dashboard", views.dashboard, name="Dashboard")
+    path("Dashboard", views.dashboard, name="Dashboard"),
+    path("Volunteers", views.volunteers, name="Volunteers")
 ]
--- LikkleDeedsOfKindness/user/views.py
+++ LikkleDeedsOfKindness/user/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, "Dashboard.html", {})
+
+def volunteers(request):
+    return render(request, "Volunteers.html", {})
+    
