9fbf3a42628c8a0af55564ed925a2e47ee38dd31
Matthew Cole <m.b.cole300@gmail.com>
2022-05-22 16:09:45 -0500

    Implemented Edit Cuase

--- LikkleDeedsOfKindness/user/urls.py
+++ LikkleDeedsOfKindness/user/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("Dashboard", views.dashboard, name="Dashboard"),
     path("Volunteers", views.volunteers, name="Volunteers"),
     path("ManageCauses", views.manage_causes, name="ManageCauses"),
-    path("AddCause", views.add_cause, name="AddCause")
+    path("AddCause", views.add_cause, name="AddCause"),
+    path("EditCause/<int:id>", views.edit_cause, name="EditCause")
 
 ]
--- LikkleDeedsOfKindness/user/views.py
+++ LikkleDeedsOfKindness/user/views.py
@@ -35,3 +35,28 @@ def add_cause(request):
         return HttpResponseRedirect(reverse("user:ManageCauses")) 
         
     return render(request, "AddCause.html", {})
+
+def edit_cause(request, id):
+
+    cause:Cause = Cause.objects.get(pk=id)
+
+    if request.method == "POST":
+
+        cause.title = request.POST["title"]
+        cause.description = request.POST["description"]
+        cause.content = request.POST["body"]
+
+        if(request.FILES):
+            cause.image = request.FILES["display_image"]
+
+        cause.save()
+
+        return HttpResponseRedirect(reverse("user:ManageCauses"))
+        
+    else:
+
+        context = {
+            "cause": cause,
+        }
+        return render(request, "AddCause.html", context)
+
