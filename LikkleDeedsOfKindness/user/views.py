e04935cae4f79f513f3712af3642a503b3997eb2
Matthew Cole <m.b.cole300@gmail.com>
2022-05-22 12:10:40 -0500

    collecting and storing cause data

--- LikkleDeedsOfKindness/user/views.py
+++ LikkleDeedsOfKindness/user/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from cause.models import Cause
 
 # Create your views here.
 
@@ -17,7 +18,14 @@ def manage_causes(request):
 def add_cause(request):
 
     if request.method == "POST":
-        #print(request.POST["title"])
+        
+        cause = Cause()
+        cause.title = request.POST["title"]
+        cause.description = request.POST["description"]
+        cause.content = request.POST["body"]
+        cause.image = request.FILES["display_image"]
+        cause.save()
+
         return HttpResponseRedirect(reverse("user:ManageCauses")) 
         
     return render(request, "AddCause.html", {})
