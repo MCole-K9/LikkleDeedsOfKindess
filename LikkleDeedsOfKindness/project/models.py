af5ced5edeb418fcca1adb740116f742e287c2c9
Matthew Cole <m.b.cole300@gmail.com>
2022-05-24 19:38:59 -0500

    added video link field

--- LikkleDeedsOfKindness/project/models.py
+++ LikkleDeedsOfKindness/project/models.py
@@ -10,7 +10,11 @@ class Project(models.Model):
     description = models.TextField()
     content = models.TextField()
     cause = models.ForeignKey(Cause, on_delete=models.CASCADE)
+    video_link = models.CharField(max_length=225, default="none")
 
+    def __str__(self):
+        return self.title
+    
 
 class ProjectImage(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
