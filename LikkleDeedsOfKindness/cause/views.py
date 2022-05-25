85a2a7c084d582dc4b1a457c4a8e43047cb452f0
Matthew Cole <m.b.cole300@gmail.com>
2022-05-25 12:48:47 -0500

    passing all cause related project to cause detail template

--- LikkleDeedsOfKindness/cause/views.py
+++ LikkleDeedsOfKindness/cause/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Cause
+from project.models import Project
 
 # Create your views here.
 
@@ -18,9 +19,11 @@ def events(request):
 def cause_detail(request, id):
 
     cause = Cause.objects.get(pk=id)
+    projects = Project.objects.filter(cause=id)
     
     context = {
         "cause": cause,
+        "projects": projects
     }
 
     return render(request, "causedetail.html", context)
