8f31e34a28c52965a27e1210394e0379eead314a
Matthew Cole <m.b.cole300@gmail.com>
2022-05-25 19:43:14 -0500

    sending project images to index(Home) and gallary

--- LikkleDeedsOfKindness/LikkleDeedsOfKindness/views.py
+++ LikkleDeedsOfKindness/LikkleDeedsOfKindness/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from cause.models import Cause, Event
-
+from project.models import ProjectImage
 
 
 
 def index(request):
 
-    causes = Cause.objects.all()
+    causes = Cause.objects.all()[:4]
+
+
+    
+    
+
+    project_images = ProjectImage.objects.all()[:6:-1]
     context = {
         "causes": causes,
+        "images": project_images
     }
 
-    return render(request, "index.html", {})   
+    return render(request, "index.html", context)   
 
 def about(request):
     return render(request, "about.html", {})
 
 def gallery(request):
-    return render(request, "gallery.html", {})
+    project_images = ProjectImage.objects.all()
+    context = {
+        "images" : project_images
+    }
+    return render(request, "gallery.html", context)
