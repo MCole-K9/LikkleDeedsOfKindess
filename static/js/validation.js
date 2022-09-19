46e7e666c9f0a93616c15e3d61da7522fcce19d6
Matthew Cole <m.b.cole300@gmail.com>
2022-09-18 20:22:21 -0500

    Validated Volunteer Forms(Consider Making it a component)

---
+++ static/js/validation.js
@@ -0,0 +1,16 @@
+// function volunteerFrmValidation(form){
+
+//     // let firstName = form.first_name;
+//     // let lastName = form.last_name;
+//     let email = form.email;
+//     let phoneNumber = form.phone_number;
+
+//     if (!email.value.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)){
+//         alert("Invalid Email Format");
+//         return false;
+
+//     }else if(!phoneNumber.value.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)){
+//         alert("invalid phone number")
+//         return false
+//     }
+// }
